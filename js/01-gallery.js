1ac8096426559ec052681b89be34154dc27632e5
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-09-12 14:56:16 +0300

    Update 01-gallery.js
    Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
    Реализация делегирования на div.gallery и получение url большого изображения.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,38 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const picturesMarkup = createGalleryCards(galleryItems);
+// const picturesLink = document.querySelector('.gallery__image');
+
+
+galleryContainer.insertAdjacentHTML('beforeend', picturesMarkup);
+
+galleryContainer.addEventListener('click', onContainerClick)
+
+function createGalleryCards(pictures) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+<div class="gallery__item">
+  <a class="gallery__link" href="#${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    }).join('');
+}
+
+
+function onContainerClick(e) {
+    const isPictureGalleryEl = e.target.classList.contains('gallery__image');
+    if (!isPictureGalleryEl) {
+        return;
+    }
+    console.log(e.target.dataset.source);
+}
