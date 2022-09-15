6cf190ab8789c87d3287c6e11af1a857cb48ca56
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-09-15 14:57:58 +0300

    Update gallery-items.js
    Додала додаткові фото для lazyloading.

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -62,4 +62,109 @@ export const galleryItems = [
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
   },
+   {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455_960_720.jpg',
+    description: 'Perfect fog in nature',
+  },
+   {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/11/13/23/06/tree-6792528_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/11/13/23/06/tree-6792528_960_720.jpg',
+    description: 'Butterflyes sunset',
+  },
+   {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/04/19/16/36/sun-2242813_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/04/19/16/36/sun-2242813_960_720.jpg',
+    description: 'Sunset sea',
+  },
+   {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/09/02/13/35/mountains-7427623__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/09/02/13/35/mountains-7427623__340.jpg',
+    description: 'Mountains fog',
+  },
+   {
+    preview:
+      'https://cdn.pixabay.com/photo/2013/11/28/10/03/river-219972__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2013/11/28/10/03/river-219972__480.jpg',
+    description: 'River in Ukraine',
+  },
+   {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/10/10/07/48/beach-2836300_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/10/10/07/48/beach-2836300_960_720.jpg',
+    description: 'Sunset in the sea',
+  },
+   {
+    preview:
+      'https://i.etsystatic.com/11810216/r/il/da0011/1210876308/il_570xN.1210876308_7hwj.jpg',
+    original:
+      'https://i.etsystatic.com/11810216/r/il/da0011/1210876308/il_570xN.1210876308_7hwj.jpg',
+    description: 'Sommer Meer Strand Palm',
+  },
+   {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_960_720.jpg',
+    description: 'White marquerite',
+  },
+   {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/05/09/03/46/alberta-2297204_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/05/09/03/46/alberta-2297204_960_720.jpg',
+    description: 'Lake Alberta in Canada',
+  },
+   {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg',
+    description: '',
+  },
+   {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/02/10/15/57/full-moon-2055469_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/02/10/15/57/full-moon-2055469_960_720.jpg',
+    description: 'Full moon shine',
+  },
+   {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/08/31/11/35/lake-moraine-2700397_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/08/31/11/35/lake-moraine-2700397_960_720.jpg',
+    description: 'Lake Moraine in Canada mountaines',
+  },
+    {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/04/20/20/23/bee-7146136_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/04/20/20/23/bee-7146136_960_720.jpg',
+    description: 'Bee in nature',
+  },
+     {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/06/19/20/13/sunset-815270__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/06/19/20/13/sunset-815270__340.jpg',
+    description: 'Sunset red',
+  },
+      {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/07/11/15/43/woman-1509956_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/07/11/15/43/woman-1509956_960_720.jpg',
+    description: 'Woman',
+  },
 ];
