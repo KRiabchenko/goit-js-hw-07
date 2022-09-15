620fd1cf6f65d5e6286de9db4138c03bd98461b1
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-09-15 15:14:09 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -48,37 +48,14 @@ function onContainerClick(e) {
     if (!isPictureGalleryEl) {
       return;
     }
-  
-
-  
 };
 
 function createBasicLightbox(e) {
   const instance = basicLightbox.create(`
     <img src="${e.target.dataset.source}">
 `)
+  
   console.log(e.target.dataset.source);
   instance.show()
-  
-  // window.addEventListener('keydown', onEscape);
+   
 }
-
-
-
-// function closeBasicLightbox(e) {
-// const instanceEl = !basicLightbox.visible;
-//   console.log(instanceEl); 
-  
-//   if (instanceEl === true) {
-//        window.removeEventListener('keydown', onEscape); 
-//   }
-// }
-
-// function onEscape(event) {
-  
-//   if (event.code === 'Escape') {
-//     console.log(event.code);
-//   }
-// }
-
-
