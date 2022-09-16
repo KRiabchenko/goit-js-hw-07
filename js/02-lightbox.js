f69cb6bd0559b29f4fb855950109b8c653c127eb
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-09-16 12:59:58 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,13 +6,12 @@ console.log(galleryItems);
 const galleryContainer = document.querySelector('.gallery');
 const picturesMarkup = createGalleryCards(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', picturesMarkup);
-galleryContainer.addEventListener('click', onImgClick);
 
 function createGalleryCards() {
     return galleryItems.map(({ preview, original, description }) => {
         return `
     <a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" "/>
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
     </a>
     `;
     }).join('');
@@ -20,12 +19,6 @@ function createGalleryCards() {
 
 const galleryImage = document.querySelectorAll('.gallery__image');
 
-function onImgClick(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
-    return;
-  } 
-}
 
 const lightbox = new SimpleLightbox('.gallery a',
   { captionDelay: 250, captionsData: "alt" });
